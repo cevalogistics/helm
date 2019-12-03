4565592fe72e88e06caaf77ee6c6132c8b7172e4
liottar <robert.liotta@cevalogistics.com>
2019-12-03 08:09:52 -0500

    Changes

--- helm-repository/init.sh
+++ helm-repository/init.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
 
-git pull  
-rm -rf ./helm-chart-sources 2>/dev/null
-mkdir ./helm-chart-sources && cd ./helm-chart-sources/
+  
 
-git clone https://github.com/cevalogistics/etcd-make-mirror.git
-git clone https://github.com/cevalogistics/multi-cluster-dns.git
+cd ../helm-repository
 
-cd ..
+helm lint ../charts/stable/*
 
-helm lint helm-chart-sources/*
-
-helm package helm-chart-sources/*
+helm package ../charts/stable/*
 
 helm repo index --url https://cevalogistics.github.io/ .
 
-git add .
-git commit -m 'Refresh'
-git push
+#git add .
+#git commit -m 'Refresh Repository'
+#git push
+
+
+ls -lR
