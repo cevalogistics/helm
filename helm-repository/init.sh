0e95cf1e756e300ced41b500b4171855ba7014af
liottar <robert.liotta@cevalogistics.com>
2019-12-03 09:33:05 -0500

    changes

--- helm-repository/init.sh
+++ helm-repository/init.sh
@@ -12,8 +12,8 @@ helm repo index --url https://cevalogistics.github.io/ . 2>/dev/null
 mkdir work  && cd work
  
 
-git config user.email "$GIT_EMAIL"
-git config user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
 
 git clone https://github.com/cevalogistics/cevalogistics.github.io
 
