73d20a4206bcc926c9180bbbfcc3dc4a95527b76
liottar <robert.liotta@cevalogistics.com>
2019-12-03 09:25:23 -0500

    test

--- helm-repository/init.sh
+++ helm-repository/init.sh
@@ -7,15 +7,14 @@ cd helm-repository
 helm lint ../charts/stable/*
 helm package ../charts/stable/*
 
-helm repo index --url https://cevalogistics.github.io/ .
+helm repo index --url https://cevalogistics.github.io/ . 2>/dev/null
 
-mkdir work
-cd work
+mkdir work  && cd work
  
 
-git config user.email "$GIT_EMAIL"
-git config user.name "$GIT_USERNAME"
-git config user.password "$GIT_PASSWORD"
+git --global config user.email "$GIT_EMAIL"
+git --global config user.name "$GIT_USERNAME"
+git --global config user.password "$GIT_PASSWORD"
 
 git clone https://github.com/cevalogistics/cevalogistics.github.io
 
@@ -25,7 +24,5 @@ cp ../../* . 2>/dev/null
 
 git add .
 git commit -m 'Refresh Repository'
-git push
 
-
-ls -lR
+git push --repo https://$GIT_USERNAME:$GIT_PASSWORD@github.com/cevalogistics/cevalogistics.github.io
