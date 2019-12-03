8eafca875418b3bcd97b9d2511618ef83633c814
liottar <robert.liotta@cevalogistics.com>
2019-12-03 09:02:24 -0500

    changes

--- helm-repository/init.sh
+++ helm-repository/init.sh
@@ -10,15 +10,16 @@ helm repo index --url https://cevalogistics.github.io/ .
 
 mkdir work
 cd work
+
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
+git config --global user.password "$GIT_PASSWORD"
+
 git clone https://github.com/cevalogistics/cevalogistics.github.io
 
 cd cevalogistics.github.io
 
 cp ../../* . 2>/dev/null
-
-git config --global user.email "$GIT_EMAIL"
-git config --global user.name "$GIT_USERNAME"
-git config --global user.password "$GIT_PASSWORD"
 
 git add .
 git commit -m 'Refresh Repository'
